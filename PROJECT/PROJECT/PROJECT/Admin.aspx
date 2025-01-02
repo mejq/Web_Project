2982fb63cfc3fe10b06f06775b581e61ff35722b
mejq <mejo_jo@outlook.com>
2025-01-02 03:01:13 +0300

    updt
    -added admin features (user editing)

--- PROJECT/PROJECT/PROJECT/Admin.aspx
+++ PROJECT/PROJECT/PROJECT/Admin.aspx
@@ -1,3 +1,116 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master_Pages/AppPage.Master" AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="PROJECT.Admin" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        <br />
+    </p>
+    <p>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="Data Source = MEJO; Initial Catalog = PROJECT; Persist Security Info=True;User ID = admin; Password=mejo1234;Encrypt=False;TrustServerCertificate=True"
+ SelectCommand="SELECT * FROM [User_Info]"></asp:SqlDataSource>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False" DataKeyNames="userID" DataSourceID="SqlDataSource1" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" AllowSorting="True" BackColor="#7BA45D" BorderColor="#57382D" Font-Bold="False" Width="1221px">
+            <Columns>
+                <asp:BoundField DataField="username" HeaderText="username" SortExpression="username" />
+                <asp:BoundField DataField="userID" HeaderText="userID" InsertVisible="False" ReadOnly="True" SortExpression="userID" />
+                <asp:BoundField DataField="passwd" HeaderText="passwd" SortExpression="passwd" />
+                <asp:BoundField DataField="email" HeaderText="email" SortExpression="email" />
+                <asp:BoundField DataField="gender" HeaderText="gender" SortExpression="gender" />
+                <asp:BoundField DataField="Role" HeaderText="Role" SortExpression="Role" />
+            </Columns>
+            <HeaderStyle BackColor="#EFB261" ForeColor="Black" />
+            <PagerStyle BackColor="#5A463F" />
+            <RowStyle BackColor="#7BA45D" />
+        </asp:GridView>
+        <asp:Button ID="btn_del" runat="server" OnClick="btn_del_Click1" Text="Button" BackColor="#5A463F" Font-Bold="True" ForeColor="White" />
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:Label ID="Label1" runat="server" BackColor="#5A463F" BorderColor="#59453F" BorderStyle="Solid" Font-Bold="True" ForeColor="White" Text="Add User" Width="734px"></asp:Label>
+    </p>
+
+&nbsp;<asp:TextBox ID="txtbx_Username" runat="server" BackColor="#7BA45D" BorderColor="#5A463F" BorderStyle="Solid" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="30px" OnTextChanged="TextBox1_TextChanged" Width="141px">Username</asp:TextBox>
+    <asp:TextBox ID="txtbx_Password" runat="server" BackColor="#7BA45D" BorderColor="#59453E" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="30px" OnTextChanged="TextBox1_TextChanged" Width="141px">Password</asp:TextBox>
+    <asp:TextBox ID="txtbx_email" runat="server" BackColor="#7BA45D" BorderColor="#5A463F" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="30px" OnTextChanged="TextBox1_TextChanged" Width="155px">Email</asp:TextBox>
+    <asp:DropDownList ID="ddl_gender" runat="server" BackColor="#7BA45D" Font-Bold="True" ForeColor="White" Height="30px">
+        <asp:ListItem>Female</asp:ListItem>
+        <asp:ListItem>Male</asp:ListItem>
+    </asp:DropDownList>
+    <asp:DropDownList ID="ddl_role" runat="server" BackColor="#7BA45D" Font-Bold="True" ForeColor="White" Height="30px" Width="78px">
+        <asp:ListItem>User</asp:ListItem>
+        <asp:ListItem>Admin</asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:Button ID="btn_add" runat="server" BackColor="#5A463F" Font-Bold="True" ForeColor="White" OnClick="btn_del_Click1" Text="Button" />
+    &nbsp;&nbsp;&nbsp; `<p>
+    </p>
+    <p>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <asp:Label ID="Label2" runat="server" BackColor="#5A463F" BorderColor="#59453F" BorderStyle="Solid" Font-Bold="True" ForeColor="White" Text="Update User" Width="734px"></asp:Label>
+    </p>
+    <p>
+    <asp:DropDownList ID="ddl_id_updt" runat="server" BackColor="#7BA45D" Font-Bold="True" ForeColor="White" Height="43px" DataSourceID="SqlDataSource1" DataTextField="userID" DataValueField="userID">
+    </asp:DropDownList>
+    </p>
+
+&nbsp;<asp:TextBox ID="txtbx_Username_updt" runat="server" BackColor="#7BA45D" BorderColor="#5A463F" BorderStyle="Solid" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="30px" OnTextChanged="TextBox1_TextChanged" Width="141px"></asp:TextBox>
+    <asp:TextBox ID="txtbx_Password_updt" runat="server" BackColor="#7BA45D" BorderColor="#59453E" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="30px" OnTextChanged="TextBox1_TextChanged" Width="141px"></asp:TextBox>
+    <asp:TextBox ID="txtbx_email_updt" runat="server" BackColor="#7BA45D" BorderColor="#5A463F" Font-Bold="True" Font-Size="Large" ForeColor="White" Height="30px" OnTextChanged="TextBox1_TextChanged" Width="155px"></asp:TextBox>
+    <asp:DropDownList ID="ddl_gender_updt" runat="server" BackColor="#7BA45D" Font-Bold="True" ForeColor="White" Height="30px">
+        <asp:ListItem>Gender</asp:ListItem>
+        <asp:ListItem>Female</asp:ListItem>
+        <asp:ListItem>Male</asp:ListItem>
+    </asp:DropDownList>
+    <asp:DropDownList ID="ddl_role_updt" runat="server" BackColor="#7BA45D" Font-Bold="True" ForeColor="White" Height="30px" Width="78px" OnSelectedIndexChanged="ddl_role_updt_SelectedIndexChanged">
+        <asp:ListItem>Role</asp:ListItem>
+        <asp:ListItem>User</asp:ListItem>
+        <asp:ListItem>Admin</asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Button" />
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
 </asp:Content>
--- PROJECT/PROJECT/PROJECT/Admin.aspx.cs
+++ PROJECT/PROJECT/PROJECT/Admin.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -10,14 +12,191 @@ namespace PROJECT
     public partial class Admin : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            
+        {    
                 if (Session["Role"] == null || Session["Role"].ToString() != "Admin")
                 {
                     Response.Redirect("Login.aspx");
                 }
-            
+                //GridView1.DeleteMethod = "Del_Usr";
+        }
+        private string connectionString = "Data Source = MEJO; Initial Catalog = PROJECT; Persist Security Info=True;" +
+                                          "User ID = admin; Password=mejo1234;Encrypt=False;TrustServerCertificate=True";    
+        
+        string query = "DELETE FROM User_Info WHERE userID=@selected_index";
+
+
+        //user silmne
+        public void Del_Usr()
+        {
+            int selected_index =Convert.ToInt32(GridView1.SelectedRow.Cells[2].Text);
+            using (SqlConnection connection=new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@selected_index", selected_index);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }          
+            }
+        }
+
+        
+
+        //user ekleme
+        public void Add_Usr()
+        {
+            string username=txtbx_Username.Text;
+            string password=txtbx_Password.Text;
+            string email=txtbx_email.Text;
+            string role=ddl_role.Text;
+            string gender=ddl_gender.Text; 
+            string query_add = "INSERT INTO User_Info (username, passwd, email, gender, Role) " +
+                        "VALUES (@username, @password, @email, @gender, @role);";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query_add, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@role", role);
+                    command.Parameters.AddWithValue("@gender", gender);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }
+            }
+
 
         }
+
+
+        //user bilgileri değiştirme
+        public void Update_Usr()
+        {
+            string username = txtbx_Username_updt.Text;
+            string password = txtbx_Password_updt.Text;
+            string email = txtbx_email_updt.Text;
+            string role = ddl_role_updt.Text;
+            string gender = ddl_gender_updt.Text;
+            int userId=Convert.ToInt32(ddl_id_updt.SelectedValue);
+            string query_usrname = "UPDATE User_Info SET username = @username  WHERE userID = @userID and @username is not null;";
+            string query_passwd = "UPDATE User_Info SET passwd = @password  WHERE userID = @userID and @password is not null;";
+            string query_email = "UPDATE User_Info SET  email = @email  WHERE userID = @userID and @email is not null;";
+            string query_role = "UPDATE User_Info SET Role = @role  WHERE userID = @userID and @role is not null;";
+            string query_gender = "UPDATE User_Info SET gender = @gender  WHERE userID = @userID and @gender is not null;";
+            //        string query_usrname = "UPDATE User_Info SET username = @username  WHERE userID = @userID and @username is not null;";
+
+            if (txtbx_Username_updt.Text != "") 
+            {
+                        using (SqlConnection connection = new SqlConnection(connectionString))//updt username
+            {
+                using (SqlCommand command = new SqlCommand(query_usrname, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@userID", userId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }
+            }
+            }
+
+            if (txtbx_Password_updt.Text != "")
+            {  
+                using (SqlConnection connection = new SqlConnection(connectionString))// updt passwd
+            {
+                using (SqlCommand command = new SqlCommand(query_passwd, connection))
+                {
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@userID", userId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }
+            }
+            }
+
+            if(txtbx_email_updt.Text !="")
+            {
+            using (SqlConnection connection = new SqlConnection(connectionString))// updt email
+            {
+                using (SqlCommand command = new SqlCommand(query_email, connection))
+                {
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@userID", userId);
+
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }
+            }
+            }
+
+            if (ddl_role_updt.SelectedIndex != 0)
+            {             using (SqlConnection connection = new SqlConnection(connectionString))// updt role
+            {
+                using (SqlCommand command = new SqlCommand(query_role, connection))
+                {
+                    command.Parameters.AddWithValue("@role", role);
+                    command.Parameters.AddWithValue("@userID", userId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }
+            }
+            }
+
+            if(ddl_gender_updt.SelectedIndex != 0) 
+            {  
+                using (SqlConnection connection = new SqlConnection(connectionString))// updt gender
+            {
+                using (SqlCommand command = new SqlCommand(query_gender, connection))
+                {
+                    command.Parameters.AddWithValue("@gender", gender);
+                    command.Parameters.AddWithValue("@userID", userId);
+
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    GridView1.DataBind();
+                }
+            }
+
+            }
+            
+
+
+
+        }
+
+
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {}
+
+        protected void btn_del_Click(object sender, EventArgs e)
+        { Del_Usr(); }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {}
+
+        protected void btn_del_Click1(object sender, EventArgs e)//btn_add
+        {Add_Usr();}
+
+        protected void Button2_Click(object sender, EventArgs e)//BTN UPDATE
+        {Update_Usr();}
+
+        protected void ddl_role_updt_SelectedIndexChanged(object sender, EventArgs e)
+        {}
     }
 }
